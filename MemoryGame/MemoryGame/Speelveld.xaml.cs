4476c3c74548cf33c896af8612d42d07a5e71560
Oreolicious <OreoStarbucks@hotmail.com>
2018-10-24 16:19:01 +0200

    Multipage
    Vanzelfsprekend

---
+++ MemoryGame/MemoryGame/Speelveld.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemoryGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Speelveld : Page
+    {
+        private const int NR_OF_COLS = 8;
+        private const int NR_OF_ROWS = 2;
+        MemoryGrid grid;
+        public Speelveld()
+        {
+            InitializeComponent();
+            grid = new MemoryGrid(GameGrid, NR_OF_COLS, NR_OF_ROWS);
+        }
+    }
+}
